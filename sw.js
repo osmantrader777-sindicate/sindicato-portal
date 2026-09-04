455959f088049ac58d427831cd54975c9985ec82
osmantrader777-sindicate <osmantrader777@gmail.com>
2026-09-03 21:11:32 -0600

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'sindicato-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
